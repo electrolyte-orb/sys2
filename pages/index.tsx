a28401d3106bf4f80d4d98e8428d94b345679f9d
Electrolyte <electrolyte.spdf@outlook.com>
2021-12-07 20:14:09 +0530

    first commit

---
+++ pages/index.tsx
@@ -0,0 +1,5 @@
+function Home() {
+	return <div className="bg-gray-100 flow-root min-h-screen"></div>;
+}
+
+export default Home;
---
+++ tailwind.config.js
@@ -0,0 +1,15 @@
+module.exports = {
+	mode: 'jit',
+	purge: ['pages/**/*.tsx'],
+	darkMode: false, // or 'media' or 'class'
+	theme: {
+		fontFamily: {
+			sans: ['Inter', 'system-ui'],
+		},
+		extend: {},
+	},
+	variants: {
+		extend: {},
+	},
+	plugins: [],
+};
