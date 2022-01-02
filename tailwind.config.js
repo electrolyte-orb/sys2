3b22da81711b05cb11d4bb0d9591027487bb077f
Electrolyte <electrolyte.spdf@outlook.com>
2022-01-02 11:30:47 +0530

    removed tailwind custom plugin

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,11 +1,8 @@
-const plugin = require('tailwindcss/plugin');
-const flattenColorPalette = require('tailwindcss/lib/util/flattenColorPalette');
 module.exports = {
 	mode: 'jit',
 	purge: ['pages/**/*.tsx', 'components/**/*.tsx'],
 	darkMode: false, // or 'media' or 'class'
 	theme: {
-		underlineColors: (theme) => flattenColorPalette.default(theme('colors')),
 		fontFamily: {
 			sans: ['Inter', 'system-ui'],
 		},
@@ -14,16 +11,5 @@ module.exports = {
 	variants: {
 		extend: {},
 	},
-	plugins: [
-		plugin(function ({ matchUtilities, theme }) {
-			matchUtilities(
-				{
-					underline: (values) => ({
-						'text-decoration-color': values,
-					}),
-				},
-				{ values: theme('underlineColors') }
-			);
-		}),
-	],
+	plugins: [],
 };
