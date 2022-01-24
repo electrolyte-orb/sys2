67a106d47003143367e15cd521cf121e428cdf1b
Electrolyte <electrolyte.spdf@outlook.com>
2022-01-24 14:51:47 +0530

    added font file

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,15 +1,25 @@
 import '../styles/globals.css';
 import type { AppProps } from 'next/app';
+import Head from 'next/head';
 import { Container, Navbar, Notice } from '../components';
 
 function MyApp({ Component, pageProps }: AppProps) {
-	return (
-		<Container>
-			<Notice />
-			<Navbar />
-			<Component {...pageProps} />
-		</Container>
-	);
+   return (
+      <>
+         <Head>
+            <link
+               rel="stylesheet"
+               href="https://rsms.me/inter/inter.css"
+               id="font-file"
+            />
+         </Head>
+         <Container>
+            <Notice />
+            <Navbar />
+            <Component {...pageProps} />
+         </Container>
+      </>
+   );
 }
 
 export default MyApp;
